ce85c975c97fd688ca84dce27c531b232b81519b
Doug <doug@bonniechef.com>
2017-06-07 22:19:14 +0100

    Fix git (remove vendor)

--- index.php
+++ index.php
@@ -6,19 +6,18 @@ error_reporting(E_ALL);
 // https://github.com/sendgrid/sendgrid-php
 // If you are using Composer (recommended)
 require 'vendor/autoload.php';
-// If you are not using Composer
-// require("path/to/sendgrid-php/sendgrid-php.php");
-$from = new SendGrid\Email("Example User", "test@example.com");
-$subject = "Sending with SendGrid is Fun";
-$to = new SendGrid\Email("Example User", "test@example.com");
-$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-print_r($response->headers());
-echo $response->body();
+
+//$apiKey = getenv('SENDGRID_API_KEY');;
+$sendgrid = new SendGrid("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");
+$email    = new SendGrid\Email();
+
+$email->addTo("doug@bonniechef.com")
+    ->setFrom("doug@bonniechef.com")
+    ->setSubject("Sending with SendGrid is Fun")
+    ->setHtml("and easy to do anywhere, even with PHP");
+
+$sendgrid->send($email);
+
 
 ?>
 <!DOCTYPE html>
